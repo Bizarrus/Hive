eccdd172660166ee736ad7a493fedb7977e180ca
Adrian Preuß <1371300+Bizarrus@users.noreply.github.com>
2024-10-27 17:30:29 +0100

    UI Optimization

--- js/lang/de.js
+++ js/lang/de.js
@@ -5,11 +5,11 @@ window.strings_de = {
     'Select file':              'Datei auswählen',
 
     /* URL-Search */
-    'Enter image URL':          'Gebe Bild-URL ein',
+    'Enter image URL':          'Gebe eine Bild-URL ein',
     'Search':                   'Suchen',
 
     /* Footer */
-    'View on Web Store':        'Ansicht im Web Store',
+    'View on Web Store':        'Im Web Store ansehen',
 
     /* Result */
     'The input is likely to be:':   'Die Eingabe ist wahrscheinlich:',
@@ -33,4 +33,8 @@ window.strings_de = {
     'Internal service error – we had a problem with our server. Please try again later.':               'Interner Servicefehler - wir hatten ein Problem mit unserem Server. Bitte versuchen Sie es später noch einmal.',
     'Service unavailable – we are temporarily offline for maintenance. Please try again later.':        'Der Dienst ist nicht verfügbar - wir sind wegen Wartungsarbeiten vorübergehend offline. Bitte versuchen Sie es später noch einmal.',
     'Gateway timeout – we are not able to fulfil your request at this time. Please try again later.':   'Gateway-Timeout - wir können Ihre Anfrage zur Zeit nicht bearbeiten. Bitte versuchen Sie es später noch einmal.',
+
+    /* Disclaimer */
+    'All rights reserved. The name Hive, the Hive logo and other resources are trademarks of <a href="https://thehive.ai/" target="_blank" class="icon-link text-decoration-none text-dark-emphasis">Castle Global, Inc.</a> and have nothing to do with this project.':
+    'Alle Rechte vorbehalten. Der Name Hive, das Hive-Logo und andere Ressourcen sind Marken von <a href="https://thehive.ai/" target="_blank" class="icon-link text-decoration-none text-dark-emphasis">Castle Global, Inc.</a> und haben nichts mit diesem Projekt zu tun.'
 };
